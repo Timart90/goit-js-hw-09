9c1c6d84905796c159ac9145835bbf4655dcb055
Timart90 <118985642+Timart90@users.noreply.github.com>
2023-08-18 15:25:30 +0000

    Deploying to gh-pages from @ Timart90/goit-js-hw-09@8b04cce60b56cad97ccdb889dbaf998db633ccce 🚀

---
+++ 01-color-switcher.d2be2502.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(d){a=setInterval((function(){return t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3,e.disabled=!0,n.disabled=!1)})),n.addEventListener("click",(function(){a=clearInterval(a),e.disabled=!1,n.disabled=!0}));var a=null;n.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.d2be2502.js.map
